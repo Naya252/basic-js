6926dc8683772ba6357b76131c6a3e29e29ba83b
Naya252 <naya252naya@gmail.com>
2023-12-18 12:33:23 +0300

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,157 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(){
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  toArr(val){
+    let data = val.toUpperCase().split('');
+    return data
+  }
+
+  toIndex(myKey, myValue) {
+    let fullKey = [];
+    let fullValue = [];
+
+    let notLetters = []
+
+    myValue.forEach((v, index) => {
+      if(this.alphabet.includes(v)){
+        let i = this.alphabet.indexOf(v);
+        fullValue.push(i);
+
+        if(fullKey.length < myValue.length){
+          myKey.forEach(k => {
+            if(this.alphabet.includes(k)){
+              let idx = this.alphabet.indexOf(k);
+              fullKey.push(idx)
+            }
+          })
+        }
+      } else {
+        notLetters.push({idx: index, value: v})
+      }
+    });
+
+    fullKey.splice(myValue.length)
+    return {fullKey, fullValue, notLetters}
+  }
+
+  calcMod26(data) {
+    let numRes = [];
+
+    data.fullValue.forEach((v, i) => {
+      if(typeof v === 'number'){
+        let sum = v + data.fullKey[i];
+        if(sum < 26) {
+          numRes.push(sum)
+        } else if(sum === 26) {
+          numRes.push(0)
+        } else {
+          numRes.push(sum % 26)
+        }
+      } else {
+        numRes.push(v)
+      }
+    })
+
+    return numRes
+  }
+
+  decCalcMod26(data) {
+    let numRes = [];
+
+    data.fullValue.forEach((v, i) => {
+      if(typeof v === 'number'){
+        let res = v - data.fullKey[i];
+        if(res < 26 && res > 0) {
+          numRes.push(res)
+        } else if(res < 0){
+          numRes.push(26 + res)
+        }
+        else if(res === 26) {
+          numRes.push(0)
+        } else {
+          numRes.push(res % 26)
+        }
+      } else {
+        numRes.push(v)
+      }
+    })
+
+    return numRes
+  }
+
+  addNotLetters(arr, notLetters){
+    notLetters.forEach(el => {
+      arr.splice(el.idx, 0, el.value)
+    })
+    
+    return arr
+  }
+
+  toStr(numRes) {
+    let res = [];
+    numRes.forEach(idx => {
+      if(typeof idx === 'number'){
+        res.push(this.alphabet.charAt(idx))
+      } else {
+        res.push(idx)
+      }      
+    })
+
+    return res.join('');
+  }
+
+  encrypt(val, key) {
+    if(val === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    // get array with letters
+    let myKey = this.toArr(key);
+    let myValue = this.toArr(val);
+
+    // get array with indexes of alphabet
+    let data = this.toIndex(myKey, myValue);
+
+    // calculate new indexes
+    let numRes = this.calcMod26(data);
+
+    // get array with all chars
+    let allArr = this.addNotLetters(numRes, data.notLetters);
+
+    // return new string
+    let res = this.toStr(allArr);
+
+    return res;
+  }
+
+  decrypt(val, key) {
+    if(val === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    // get array with letters
+    let myKey = this.toArr(key);
+    let myValue = this.toArr(val);
+
+    // get array with indexes of alphabet
+    let data = this.toIndex(myKey, myValue);
+
+    // calculate new indexes
+    let numRes = this.decCalcMod26(data);
+
+    // get array with all chars
+    let allArr = this.addNotLetters(numRes, data.notLetters);
+
+    // return new string
+    let res = this.toStr(allArr);
+
+    return res;
   }
 }
-
 module.exports = {
   VigenereCipheringMachine
 };
